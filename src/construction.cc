0de42ef30c055e82e156365c1a819ceb28dc67b8
hkumi <60217793+hkumi@users.noreply.github.com>
2024-05-09 14:33:30 +0200

    Update construction.cc

--- src/construction.cc
+++ src/construction.cc
@@ -736,14 +736,14 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 
   LeadSize = 3*cm;
   Lead_Box2 = new G4Box("Lead2",                             //its name
-                   30*cm/2,LeadSize/2,23*cm/2);   //its dimensions
+                   30*cm/2,LeadSize/2,33*cm/2);   //its dimensions
 
   Lead_LV2 = new G4LogicalVolume(Lead_Box2,                     //its shape
                               leadMaterial,                      //its material
                              "Lead2");                  //its name
 
   Lead_PV2 = new G4PVPlacement(0,                          //no rotation
-                            G4ThreeVector(0*cm,16.5*cm,11.5*cm),            //at (0,0,0)
+                            G4ThreeVector(0*cm,16.5*cm,16.5*cm),            //at (0,0,0)
                              Lead_LV2,                      //its logical volume
                             "Lead2",                    //its name
                             fLBox,                          //its mother  volume
@@ -763,7 +763,7 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
      //The lead3
  
   Lead_Box3 = new G4Box("Lead3",                             //its name
-                   3*cm/2,30*cm/2, 23*cm/2);   //its dimensions
+                   3*cm/2,30*cm/2, 33*cm/2);   //its dimensions
 
   Lead_LV3 = new G4LogicalVolume(Lead_Box3,                     //its shape
                               leadMaterial,                      //its material
@@ -784,14 +784,14 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
   //The lead4
 
   Lead_Box4 = new G4Box("Lead4",                             //its name
-                   3*cm/2,30*cm/2, 23*cm/2);   //its dimensions
+                   3*cm/2,30*cm/2, 33*cm/2);   //its dimensions
 
   Lead_LV4 = new G4LogicalVolume(Lead_Box4,                     //its shape
                               leadMaterial,                      //its material
                              "Lead4");                  //its name
 
   Lead_PV4 = new G4PVPlacement(0,                          //no rotation
-                            G4ThreeVector(-16.5*cm,0*cm,11.5*cm),            //at (0,0,0)
+                            G4ThreeVector(-16.5*cm,0*cm,16.5*cm),            //at (0,0,0)
                              Lead_LV4,                      //its logical volume
                             "Lead4",                    //its name
                             fLBox,                          //its mother  volume
